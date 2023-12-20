816fabcc0f4603a4abae46ba900a7cb773878d48
Alejandro Avagnina <avatxus@gmail.com>
2023-12-20 12:51:53 -0300

    chore: update gateway and kong crds (#1)

--- modules/common/gateway-api/stage1/main.tf
+++ modules/common/gateway-api/stage1/main.tf
@@ -1,18 +1,27 @@
-data "http" "gateway_crds_raw" {
-  url = "https://github.com/kubernetes-sigs/gateway-api/releases/download/v0.7.1/experimental-install.yaml"
+data "external" "process_standard_yaml" {
+  program = ["python3", abspath("${path.module}/parse_yaml.py"), "${path.module}/standard.yaml"]
+}
+
+data "external" "process_experimenta_yaml" {
+  program = ["python3", abspath("${path.module}/parse_yaml.py"), "${path.module}/experimental.yaml"]
+}
+
+data "external" "process_kong_yaml" {
+  program = ["python3", abspath("${path.module}/parse_yaml.py"), "${path.module}/kong.yaml"]
 }
 
 locals {
-  crds_raw = split("---\n", file("${path.module}/crds.yaml"))
-  # remove first element since it's not a CRD
-  crds_tmp = slice(local.crds_raw, 1, length(local.crds_raw))
-  # remove status field from each CRD
-  crds_clean = { for x in local.crds_tmp : x => yamlencode(
-    { for root_key, root_values in yamldecode(x) : root_key => root_values if root_key != "status" }
-  ) }
+  crds = merge(
+    data.external.process_standard_yaml.result,
+    data.external.process_experimenta_yaml.result,
+    data.external.process_kong_yaml.result,
+  )
+
+  # crds_old = data.external.process_old_yaml.result
 }
 
 resource "kubernetes_manifest" "gateway_crds" {
-  for_each = { for x in local.crds_clean : "${yamldecode(x).metadata.name}-${yamldecode(x).kind}" => x }
+  for_each = local.crds
+
   manifest = yamldecode(each.value)
 }
