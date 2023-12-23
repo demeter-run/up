d1e17d9d363302cf20d2e4ac9284bbb2bebeead8
Santiago Carmuega <santiago@carmuega.me>
2023-12-23 17:42:48 -0300

    fix: fix stage1 dmtr namespace (#6)

--- stage1/main.tf
+++ stage1/main.tf
@@ -19,6 +19,12 @@ provider "helm" {
   }
 }
 
+resource "kubernetes_namespace_v1" "dmtr" {
+  metadata {
+    name = var.dmtr_namespace
+  }
+}
+
 module "aws_elb_controller" {
   source       = "../modules/aws/elb-controller/stage1"
   count        = var.cloud_provider == "aws" ? 1 : 0
@@ -30,20 +36,21 @@ module "aws_external_snapshoter" {
   count  = var.cloud_provider == "aws" ? 1 : 0
 }
 
-module "metrics_server" {
-  source = "../modules/common/metrics-server/stage1"
-}
+# module "metrics_server" {
+#   source = "../modules/common/metrics-server/stage1"
+# }
 
-module "cert_manager" {
-  source = "../modules/common/cert-manager/stage1"
-}
+# module "cert_manager" {
+#   source = "../modules/common/cert-manager/stage1"
+# }
 
 module "gateway" {
   source = "../modules/common/gateway-api/stage1"
 }
 
 module "o11y" {
-  source = "../modules/common/o11y/stage1"
+  source    = "../modules/common/o11y/stage1"
+  namespace = var.dmtr_namespace
 }
 
 module "dmtrd" {
