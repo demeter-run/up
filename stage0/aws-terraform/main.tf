792d7c498b4ebb361e3502669734adddc9ae5bec
Chris Gianelloni <wolf31o2@blinklabs.io>
2024-01-23 17:04:10 -0500

    feat: irsa for aws-load-balancer-controller
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- stage0/aws-terraform/main.tf
+++ stage0/aws-terraform/main.tf
@@ -104,6 +104,23 @@ data "aws_subnets" "filtered" {
   }
 }
 
+module "aws_cluster_elb_controller_irsa" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name                              = "load-balancer-controller"
+  attach_load_balancer_controller_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.aws_cluster_eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+
+  tags = local.tags
+}
+
 module "aws_cluster_vpc_cni_irsa" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
   version = "~> 5.0"
