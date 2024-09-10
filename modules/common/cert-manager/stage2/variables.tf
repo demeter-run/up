6010f61452af496c6e6c69bfe15f3c53f1192c48
Ales Verbic <verbotenj@blinklabs.io>
2024-09-10 08:37:13 -0400

    feat: add cluster_issuer support for CloudFlare to cert-manager
    Signed-off-by: Ales Verbic <verbotenj@blinklabs.io>

--- modules/common/cert-manager/stage2/variables.tf
+++ modules/common/cert-manager/stage2/variables.tf
@@ -7,3 +7,13 @@ variable "ingress_class" {
   type    = string
   default = "alb"
 }
+
+variable "cloudflare_token" {
+  type        = string
+  default     = null
+  description = <<EOF
+Optional Cloudflare API token.
+If provided, will use Cloudflare for DNS challenges.
+Otherwise, it defaults to AWS Route53.
+EOF
+}
