45979a8fcb95a642d22dd834d7eac436d4fb368e
Overcooked Panda <overcookedpanda@gmail.com>
2024-11-08 10:11:01 -0600

    chore: update providers according to terraform 1.9.8

---
+++ stage0/gcp-terraform/versions.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.9.8"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.10.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.33.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.3"
+    }
+  }
+}
