a1fb513c06792da40e2b24daf7ff4935922730c8
Ales Verbic <verbotenj@blinklabs.io>
2025-02-18 10:45:58 -0500

    fix(utxorpc): update utxorpc module to upstream
    Signed-off-by: Ales Verbic <verbotenj@blinklabs.io>

--- stage3/utxorpc.tf
+++ stage3/utxorpc.tf
@@ -1,39 +1,15 @@
 locals {
   // Defaults for UtxoRPC
   utxorpc_defaults = {
-    namespace             = "ext-utxorpc-m0"
-    dns_zone              = "dmtr.host"
-    operator_image_tag    = "0ecf1644a0554d3c34f4256b6765c80a02986e15"
-    networks              = ["cardano-mainnet", "cardano-preprod", "cardano-preview"]
-    cloudflared_image_tag = "latest"
-    cloudflared_replicas  = 0
-    cloudflared_resources = {
-      limits = {
-        cpu    = "2"
-        memory = "500Mi"
-      }
-      requests = {
-        cpu    = "50m"
-        memory = "500Mi"
-      }
+    namespace          = "ext-utxorpc-m0"
+    dns_zone           = "dmtr.host"
+    operator_image_tag = "0ecf1644a0554d3c34f4256b6765c80a02986e15"
+    networks           = ["cardano-mainnet", "cardano-preprod", "cardano-preview"]
+    extension_url_per_network = {
+      "cardano-mainnet" = "utxorpc-m0.demeter.run"
+      "cardano-preprod" = "utxorpc-preprod.demeter.run"
+      "cardano-preview" = "utxorpc-preview.demeter.run"
     }
-    cloudflared_tolerations = [
-      {
-        effect   = "NoSchedule"
-        key      = "demeter.run/compute-profile"
-        operator = "Exists"
-      },
-      {
-        effect   = "NoSchedule"
-        key      = "demeter.run/compute-arch"
-        operator = "Exists"
-      },
-      {
-        effect   = "NoSchedule"
-        key      = "demeter.run/availability-sla"
-        operator = "Exists"
-      }
-    ]
     proxies_image_tag = "7941eec9a681ace6fdaeb7e18d9fb2b26d3cfae3"
     proxies_replicas  = 1
     proxies_resources = {
@@ -64,12 +40,9 @@ locals {
         operator = "Exists"
       }
     ]
-    cloudflared_tunnel_id     = "default-tunnel-id"
-    cloudflared_tunnel_secret = "default-tunnel-secret"
-    cloudflared_account_tag   = "default-account-tag"
-    dolos_version             = "sha-e0f42cf"
-    extension_subdomain       = "utxorpc"
-    api_key_salt              = "this is a random generated key and must be shared..."
+    dolos_version       = "sha-e0f42cf"
+    extension_subdomain = "utxorpc"
+    api_key_salt        = "this is a random generated key and must be shared..."
   }
 }
 
@@ -82,27 +55,17 @@ module "ext_cardano_utxorpc" {
   source   = "git::https://github.com/demeter-run/ext-cardano-utxorpc//bootstrap/"
   for_each = toset([for n in toset(["v1"]) : n if var.enable_cardano_utxorpc])
 
-  operator_image_tag  = local.utxorpc_defaults.operator_image_tag
-  extension_subdomain = local.utxorpc_defaults.extension_subdomain
-  dns_zone            = local.utxorpc_defaults.dns_zone
-  api_key_salt        = local.utxorpc_defaults.api_key_salt
-  namespace           = local.utxorpc_defaults.namespace
-  networks            = local.utxorpc_defaults.networks
+  operator_image_tag        = local.utxorpc_defaults.operator_image_tag
+  extension_url_per_network = local.utxorpc_defaults.extension_url_per_network
+  api_key_salt              = local.utxorpc_defaults.api_key_salt
+  namespace                 = local.utxorpc_defaults.namespace
+  networks                  = local.utxorpc_defaults.networks
 
   network_addresses = {
     # "cardano-mainnet" : "relay.utxorpc-m0.demeter.run:3000"
     # "cardano-preprod" : "relay.utxorpc-m0.demeter.run:3001"
     # "cardano-preview" : "relay.utxorpc-m0.demeter.run:3002"
   }
-
-  // Cloudflared
-  cloudflared_tunnel_id     = local.utxorpc_defaults.cloudflared_tunnel_id
-  cloudflared_tunnel_secret = local.utxorpc_defaults.cloudflared_tunnel_secret
-  cloudflared_account_tag   = local.utxorpc_defaults.cloudflared_account_tag
-  cloudflared_image_tag     = local.utxorpc_defaults.cloudflared_image_tag
-  cloudflared_replicas      = local.utxorpc_defaults.cloudflared_replicas
-  cloudflared_resources     = local.utxorpc_defaults.cloudflared_resources
-  cloudflared_tolerations   = local.utxorpc_defaults.cloudflared_tolerations
 
   // Proxies
   proxies_image_tag   = local.utxorpc_defaults.proxies_image_tag
