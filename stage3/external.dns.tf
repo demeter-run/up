9793b0b585f617eed164fbd11a743328facb10b1
Ales Verbic <verbotenj@blinklabs.io>
2025-08-31 12:30:13 -0400

    chore: update external-dns
    Signed-off-by: Ales Verbic <verbotenj@blinklabs.io>

--- stage3/external.dns.tf
+++ stage3/external.dns.tf
@@ -3,7 +3,7 @@ resource "helm_release" "external_dns" {
   name       = "external-dns"
   repository = "https://kubernetes-sigs.github.io/external-dns/"
   chart      = "external-dns"
-  version    = "1.15.1"
+  version    = "1.18.0"
   namespace  = "kube-system"
 
   values = [
